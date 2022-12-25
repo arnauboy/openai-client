19e85b43e036748b1e13d7fc2a13bac0ab57a6a7
ArnaudMegaDude <arnauboy@gmail.com>
2022-12-25 21:20:49 +0100

    user input

--- qa.py
+++ qa.py
@@ -4,10 +4,12 @@ from api_keys import API_KEY
 
 openai.api_key = API_KEY()
 
+request = input("Enter what you want to say to ChatGPT:\n")
+
 response = openai.Completion.create(
   model="text-davinci-003",
-  prompt="Say this is a test",
+  prompt=request,
   max_tokens=100,
 )
 
-print(response)
+print(response.choices[0].text)
